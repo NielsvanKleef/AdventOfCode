89829005b54facab04ec2d8974ba4fee2b38215d
NielsvanKleef <nj.vankleef@gmail.com>
2024-12-01 22:45:14 +0100

    Day 1 puzzle 2 solution

--- 2024/src/controller/Day1Launcher.java
+++ 2024/src/controller/Day1Launcher.java
@@ -9,26 +9,23 @@ public class Day1Launcher {
     public static List<Integer> leftList = new ArrayList<>();
     public static List<Integer> rightList = new ArrayList<>();
 
-    public static List<Integer> sampleLeftList = Arrays.asList(3, 4, 2, 1, 3, 3);
-    public static List<Integer> sampleRightList = Arrays.asList(4, 3, 5, 3, 9, 3);
-
     public static void main(String[] args) {
         List<String> inputLines = FileService.readFileLines("AdventOfCode/2024/resources/day1puzzle1.txt");
 
         for (String inputLine : inputLines) {
-            String string[] = inputLine.split(" ");
+            String[] string = inputLine.split(" ");
             leftList.add(Integer.parseInt(string[0]));
             rightList.add(Integer.parseInt(string[3]));
         }
 
         System.out.println(computeTotalDistance(leftList, rightList));
 
-        System.out.println(computeSimilarityScore(mapNumberOfOccurrence(sampleLeftList, sampleRightList), sampleLeftList));
+        System.out.println(computeSimilarityScore(mapNumberOfOccurrence(leftList, rightList), leftList));
 
     }
 
     public static Map<Integer, Long> mapNumberOfOccurrence(List<Integer> leftList, List<Integer> rightList) {
-        Set<Integer> numbersInLeftList = new HashSet<Integer>(leftList);
+        Set<Integer> numbersInLeftList = new HashSet<>(leftList);
         Map<Integer, Long> numberOfOccurrenceMap = new HashMap<>();
         for (Integer number : numbersInLeftList) {
             Long count = rightList.stream()
@@ -40,8 +37,8 @@ public class Day1Launcher {
         return numberOfOccurrenceMap;
     }
 
-    public static Long computeSimilarityScore(Map<Integer, Long> map, List<Integer> leftList) {
-        Long score = 0L;
+    public static long computeSimilarityScore(Map<Integer, Long> map, List<Integer> leftList) {
+        long score = 0L;
         for (Integer leftListEntry : leftList) {
             Long entry = map.get(leftListEntry);
             if (entry != null) {
